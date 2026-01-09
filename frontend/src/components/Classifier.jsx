211e61a716816176b06eddf3f4fa9741bec302ab
FBahtiar <fahrybahtiar7@gmail.com>
2026-01-09 12:33:27 +0700

    update frontend

--- frontend/src/components/Classifier.jsx
+++ frontend/src/components/Classifier.jsx
@@ -70,7 +70,7 @@ const Classifier = () => {
 
         try {
             // Replace with your actual backend URL or localhost
-            const response = await axios.post('http://localhost:8000/classify', formData, {
+            const response = await axios.post('https://3362dc70501c.ngrok-free.app/classify', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
